682ad04090bf148e81dd8feb7b71f135feb759df
zhanglei <lei.zhang@sunscell.com>
2016-08-24 12:53:15 +0800

    merge

---
+++ AppClient/MainPage.xaml.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.ApplicationModel.AppService;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Popups;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+//“空白页”项模板在 http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409 上有介绍
+
+namespace AppClient
+{
+    /// <summary>
+    /// 可用于自身或导航至 Frame 内部的空白页。
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        //声明一个AppService连接对象
+        private readonly AppServiceConnection connection = new AppServiceConnection
+        {
+            AppServiceName = "appServicex",
+            PackageFamilyName = "6fdb4336-44bf-47d4-8386-075b1ec51d04_wbwwzgv8bqypp"
+        };
+        public MainPage()
+        {
+            this.InitializeComponent();
+            this.Loaded += MainPage_Loaded;
+        }
+        private async void MainPage_Loaded(object sender, RoutedEventArgs e)
+        {
+            var connectionStatus = await connection.OpenAsync();
+            if (connectionStatus == AppServiceConnectionStatus.Success)
+            {
+                //订阅双向通信
+                connection.RequestReceived += Connection_RequestReceived;
+            }
+            else
+            {
+                //可以提示并导航到商店 让用户下载该服务
+                await new MessageDialog("服务连接不成功").ShowAsync();
+            }
+        }
+        private void Connection_RequestReceived(AppServiceConnection sender, AppServiceRequestReceivedEventArgs args)
+        {
+            // To do something
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var message = new ValueSet();
+            message.Add("Command", "CalcSum");
+            message.Add("Value1", int.Parse(txtNum1.Text));
+            message.Add("Value2", int.Parse(txtNum2.Text));
+            AppServiceResponse response = await connection.SendMessageAsync(message);
+            if (response.Status == AppServiceResponseStatus.Success)
+            {
+                string sum = response.Message["Rusult"].ToString();
+                await new MessageDialog(sum).ShowAsync();
+            }
+            else
+            { }
+        }
+    }
+}
---
+++ AppServiceTasks/AppServiceTask.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.ApplicationModel.AppService;
+using Windows.ApplicationModel.Background;
+using Windows.Foundation.Collections;
+
+namespace AppServiceTasks
+{
+    public sealed class AppServiceTask : IBackgroundTask
+    {
+        private static BackgroundTaskDeferral _serviceDeferral;
+        public void Run(IBackgroundTaskInstance taskInstance)
+        {
+            //订阅关闭事件
+            taskInstance.Canceled += TaskInstance_Canceled; ;
+            _serviceDeferral = taskInstance.GetDeferral();
+            var appService = taskInstance.TriggerDetails as AppServiceTriggerDetails;
+            //验证调用者
+            if (appService?.Name == "appServicex")
+            {
+                //订阅调用者请求
+                appService.AppServiceConnection.RequestReceived += AppServiceConnection_RequestReceived;
+            }
+        }
+
+        private void TaskInstance_Canceled(IBackgroundTaskInstance sender, BackgroundTaskCancellationReason reason)
+        {
+            throw new NotImplementedException();
+        }
+
+        private async void AppServiceConnection_RequestReceived(AppServiceConnection sender, AppServiceRequestReceivedEventArgs args)
+        {
+            Debug.Write(".................");
+            var message = args.Request.Message;
+            string command = message["Command"] as string;
+            switch (command)
+            {
+                case "CalcSum":
+                    var messageDeferral = args.GetDeferral();
+                    int value1 = (int)message["Value1"];
+                    int value2 = (int)message["Value2"];
+                    var returnMessage = new ValueSet();
+                    returnMessage.Add("Rusult", value1 + value2);
+                    //回应调用者
+                    var responseStatus = await args.Request.SendResponseAsync(returnMessage);
+                    messageDeferral.Complete();
+                    break;
+                case "Quit":
+                    _serviceDeferral.Complete();
+                    break;
+            }
+        }
+    }
+}
